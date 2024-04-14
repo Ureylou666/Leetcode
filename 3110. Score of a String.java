476c3d6eaeddf15ad11ba18db47bc514c1aec28a
娄墩墩 Lou Dwen Dwen <75350727+Ureylou666@users.noreply.github.com>
2024-04-14 19:29:51 +0800

    Create 3110. Score of a String.java

---
+++ 3110. Score of a String.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int scoreOfString(String s) {
+        char[] chars = s.toCharArray();
+        int result = 0;
+        for (int i=0;i<chars.length-1;i++) result = result + (int) Math.abs(chars[i] - chars[i+1]);
+        return result;
+    }
+}
