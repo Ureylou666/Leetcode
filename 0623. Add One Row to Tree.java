0e8eaa66443b4fc7ebec9966d4e5c18736ca60e2
娄墩墩 Lou Dwen Dwen <75350727+Ureylou666@users.noreply.github.com>
2024-04-16 15:31:46 +0800

    Update 0623. Add One Row to Tree.java

--- 0623. Add One Row to Tree.java
+++ 0623. Add One Row to Tree.java
@@ -43,3 +43,55 @@ class Solution {
         return root;
     }
 }
+
+---
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (depth == 1) {
+            TreeNode newRoot = new TreeNode(val, root, null);
+            return newRoot;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int currentDepth = 1;
+
+        // Traverse to the level just before the target depth
+        while (currentDepth < depth - 1) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            currentDepth++;
+        }
+
+        // Add one row
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode newLeft = new TreeNode(val, node.left, null);
+            TreeNode newRight = new TreeNode(val, null, node.right);
+            node.left = newLeft;
+            node.right = newRight;
+        }
+
+        return root;
+    }
+}
