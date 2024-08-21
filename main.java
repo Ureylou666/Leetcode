f6f0326616f753dcd15ccd800c6b53f963dcc12e
ureylou666 <ureylou@gmail.com>
2024-08-20 20:04:13 -0700

    20240820

--- main.java
+++ main.java
@@ -1,9 +1,22 @@
 class main {
 
     public static void main (String args[]) {
-        System.out.println(  minSteps(74) );
+        System.out.println(  stoneGameII(new int[]{1,2,3,4,100}) );
     }
 
+
+    
+    /**
+    * @Description: https://leetcode.com/problems/stone-game-ii/description/
+    * @Author: Urey Lou
+    * @Date: 2024-08-20
+    * @Param: [piles]
+    * @return: int
+    */
+    public static int stoneGameII(int[] piles) {
+
+
+    }
 
 
 }
