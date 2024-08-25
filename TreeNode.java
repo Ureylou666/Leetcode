0313379c1c1a82b116c9494edc7e9f0e1b9b7f75
ureylou666 <ureylou@gmail.com>
2024-08-25 14:50:52 -0700

    20240824 contest

--- TreeNode.java
+++ TreeNode.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TreeNode {
      int val;
      TreeNode left;
@@ -9,4 +12,7 @@ public class TreeNode {
          this.left = left;
          this.right = right;
      }
+
+
+
  }
--- main.java
+++ main.java
@@ -2,9 +2,51 @@ import java.util.*;
 
 class main {
 
-    public static void main (String args[]) {
-        System.out.println(  );
+    public static void main (String[] args) {
+        System.out.println( postorderTraversal( arrayToTreeNode( new Integer[]{1,null,2,3}  )  ) );
     }
 
+    /**
+     * @Description: arrayToTreeNode
+     * @Author: Urey Lou
+     * @Date: 2024-08-25
+     * @Param: [inputs]
+     * @return: TreeNode
+     */
+    public static TreeNode arrayToTreeNode(Integer[] inputs) {
+        if (inputs == null || inputs.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(inputs[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int i = 1;
+        while (i < inputs.length) {
+            TreeNode currentNode = queue.poll();
+            if (inputs[i] != null) {
+                currentNode.left = new TreeNode(inputs[i]);
+                queue.offer(currentNode.left);
+            }
+            i++;
+            if (i < inputs.length && inputs[i] != null) {
+                currentNode.right = new TreeNode(inputs[i]);
+                queue.offer(currentNode.right);
+            }
+            i++;
+        }
+        return root;
+    }
+
+    /** 
+    * @Description:
+    * @Author: Urey Lou
+    * @Date: 2024-08-25
+    * @Param: [root]
+    * @return: java.util.List<java.lang.Integer>
+    */
+
+    public static List<Integer> postorderTraversal(TreeNode root) {
+
+    }
 
 }
