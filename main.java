79600564298903c98000b55a86e1b4d9580057fe
ureylou666 <ureylou@gmail.com>
2024-08-24 21:05:04 -0700

    20240824 contest

--- main.java
+++ main.java
@@ -1,23 +1,10 @@
-import java.util.Arrays;
+import java.util.*;
 
 class main {
 
     public static void main (String args[]) {
-        System.out.println(  nearestPalindromic("12" ));
+        System.out.println(  );
     }
-
-
-    /**
-    * @Description: https://leetcode.com/problems/stone-game-ii/description/
-    * @Author: Urey Lou
-    * @Date: 2024-08-20
-    * @Param: [piles]
-    * @return: int
-    */
-   //public static int stoneGameII(int[] piles) {
-
-
-   //}
 
 
 }
