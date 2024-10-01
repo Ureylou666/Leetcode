a0f0546e907be6591f0a98adf497039cc23d9540
ureylou666 <ureylou@gmail.com>
2024-09-30 23:01:22 -0700

    20240930

--- main.java
+++ main.java
@@ -6,7 +6,14 @@ class main {
         // String grid1 = "[[1,0,1,0,1],[1,1,1,1,1],[0,0,0,0,0],[1,1,1,1,1],[1,0,1,0,1]]";
         // String grid2 = "[[0,0,0,0,0],[1,1,1,1,1],[0,1,0,1,0],[0,1,0,1,0],[1,0,0,0,1]]";
         List<List<Integer>> triangle = new ArrayList<>();
+        System.out.println( zigzagLevelOrder( Utilities.arrayToTreeNode(new Integer[]{1,2,3,4,null,null,5}) ) );
     }
+
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+    }
+
 
     public static boolean isInterleave(String s1, String s2, String s3) {
         if (s1.length() + s2.length() != s3.length()) return false;
@@ -28,9 +35,6 @@ class main {
         return dp[n];
     }
 
-    public List<List<String>> partition(String s) {
-
-    }
 
     /*
     public static boolean isInterleave(String s1, String s2, String s3) {
