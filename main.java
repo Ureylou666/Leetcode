a3078988ba2a52505163c97d779f426765ec9942
ureylou666 <ureylou@gmail.com>
2024-10-11 22:12:18 -0700

    20241011

--- main.java
+++ main.java
@@ -4,9 +4,8 @@ class main {
 
     public static void main(String[] args) {
         // String grid2 = "[[0,0,0,0,0],[1,1,1,1,1],[0,1,0,1,0],[0,1,0,1,0],[1,0,0,0,1]]";
-        System.out.println( );
+        System.out.println(grayCode(3) );
     }
-
 
 
 }
